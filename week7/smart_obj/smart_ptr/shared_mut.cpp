8d7b13a1a98a6724c8c11705d457a323de3fa59d
MinhawnKim <201802061@o.cnu.ac.kr>
2019-10-29 06:50:04 +0900

    d

---
+++ week7/smart_obj/smart_ptr/shared_mut.cpp
@@ -0,0 +1,62 @@
+//
+// Created by 김혁진 on 14/10/2019.
+//
+
+#include "shared_mut.h"
+
+namespace ptr {
+shared_mut::shared_mut() {
+    _mgr = new mgr();
+}
+
+shared_mut::shared_mut(Object* _obj) {
+    _mgr = new mgr(_obj);
+}
+
+shared_mut::~shared_mut() {
+    release();
+}
+void shared_mut::release(){
+    _mgr->count = _mgr->count-1;
+    if(_mgr->count == 0){
+	delete(_mgr);
+    }
+}
+Object* shared_mut::get() const {
+    if(_mgr == nullptr){
+	return nullptr;
+    }
+    return _mgr->ptr;
+
+}
+int shared_mut::count() {
+    return _mgr->count;
+}
+void shared_mut::increase() {
+    _mgr->count = _mgr->count+1;
+}
+shared_mut shared_mut::operator+(const shared_mut &shared){
+     shared_mut n(new Object(get()->get() + shared.get()->get()));
+    return n;
+}
+shared_mut shared_mut::operator-(const shared_mut &shared){
+ shared_mut n(new Object(get()->get() - shared.get()->get()));
+    return n;
+}
+shared_mut shared_mut::operator*(const shared_mut &shared){
+ shared_mut n(new Object(get()->get() * shared.get()->get()));
+    return n;
+}
+shared_mut shared_mut::operator/(const shared_mut &shared){
+ shared_mut n(new Object(get()->get() / shared.get()->get()));
+    return n;
+}
+Object* shared_mut::operator->(){
+    return _mgr->ptr;
+}
+shared_mut& shared_mut::operator=(const shared_mut &r){
+    release();
+    _mgr = r._mgr;
+    increase();
+}
+} // end of namespace ptr
---
+++ week7/smart_obj/smart_ptr/unique_immut.cpp
@@ -0,0 +1,76 @@
+//
+// Created by 김혁진 on 14/10/2019.
+//
+
+#include "unique_immut.h"
+
+namespace ptr {
+unique_immut::unique_immut() {
+    _mgr = new mgr();
+}
+
+unique_immut::unique_immut(Object *obj) {
+    _mgr = new mgr(obj);
+}
+
+unique_immut::unique_immut(const unique_immut &immut) {
+    if (immut._mgr) {
+        int val = immut._mgr->ptr->get();
+        _mgr = new mgr(val);
+    }
+}
+
+unique_immut::~unique_immut() {
+    release();
+}
+void unique_immut::release(){
+    if(_mgr != nullptr){
+	delete(_mgr);
+    }
+}
+Object* unique_immut::get() const{
+    if(_mgr == nullptr){
+        return nullptr;
+    }
+    return _mgr->ptr;
+}
+
+unique_immut unique_immut::operator+(unique_immut &unique){
+    int val = get()->get() + unique.get()->get();
+    release();
+    unique.release();
+    unique_immut ui(new Object(val));
+    return ui;
+}
+unique_immut unique_immut::operator-(unique_immut &unique){
+    int val = get()->get() - unique.get()->get();
+    unique_immut ui(new Object(val));
+    release();
+    unique.release();
+    return ui;
+}
+unique_immut unique_immut::operator*(unique_immut &unique){
+    int val = get()->get() * unique.get()->get();
+    unique_immut ui(new Object(val));
+    release();
+    unique.release();
+    return ui;
+}
+unique_immut unique_immut::operator/(unique_immut &unique){
+    int val = get()->get() / unique.get()->get();
+    unique_immut ui(new Object(val));
+    release();
+    unique.release();
+    return ui;
+}
+Object* unique_immut::operator->(){
+    return _mgr->ptr;
+}
+unique_immut& unique_immut::operator=(unique_immut& r){
+    if(_mgr == r._mgr){
+	return *this;
+    }
+    release();
+    return r;
+}
+} // end of namespace ptr
